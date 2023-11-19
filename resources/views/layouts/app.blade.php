38c7c9fb9bbeb96f9558fb7c5161941d09be0bfa
Luis Dominguez <luisdominguez818@gmail.com>
2023-11-19 11:53:17 -0500

    novedades first commit

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>@yield('title')</title>
+    @vite('resources/css/app.css')
+
+    <!-- Fonts -->
+    <link rel="preconnect" href="https://fonts.bunny.net">
+    <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
+</head>
+<body>
+    <main>
+        @yield('content')
+    </main>
+    @vite('resources/js/app.js')
+</body>
+</html>
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.app')
+
+@section('title', 'Welcome App')
+
+@section('content')
+    <h1>Bundle 10</h1>
+@endsection
